18b36b93bd1cce8180de3d27531c349e7f8e5790
Pattama Kaewsopa <nep.us@hotmail.com>
2015-05-12 16:01:38 +0700

    - Change Vehicle information fragment - Add Scroll area(Vehicle)

---
+++ app/src/main/java/com/example/nepus/driverassistance/ViewPagerFragment.java
@@ -0,0 +1,12 @@
+package com.example.nepus.driverassistance;
+
+/**
+ * Created by NEPUS on 5/4/15 AD.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public class ViewPagerFragment extends FragmentActivity{
+}
