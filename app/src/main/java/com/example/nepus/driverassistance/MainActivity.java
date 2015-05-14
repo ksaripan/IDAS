6bd9b28428f61ac01ec6837418fe67225b7da787
Pattama Kaewsopa <nep.us@hotmail.com>
2015-05-14 20:02:01 +0700

    Revert "Edit how to use fragment."
    This reverts commit 0ec25a9746b1578833dca64b0509a3598f09b680.

--- app/src/main/java/com/example/nepus/driverassistance/MainActivity.java
+++ app/src/main/java/com/example/nepus/driverassistance/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
     private static final String TAG = MainActivity.class.getSimpleName();
     private NavigationDrawerFragment navDrawerFrag;
     private CharSequence title;
-//    private static PlaceholderFragment placeholderFragment = null;
 
 
     private static final int MESSAGE_REFRESH = 101;
@@ -84,7 +83,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 
         }
     };
-    private Fragment[] fragments;
 
 
     private void refreshDeviceList() {
@@ -131,12 +129,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         }.execute((Void) null);
     }
 
-    public MainActivity() {
-        fragments = new Fragment[5];
-        fragments[0] = new TestFragment();
-        fragments[1] = new TestFragment2();
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -146,8 +138,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 
         //Navigation drawer
         navDrawerFrag.setUp(R.id.navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout));
-
-
 
 
     }
@@ -164,22 +154,13 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         mHandler.removeMessages(MESSAGE_REFRESH);
     }
 
-
-
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        if (position > 1)
-            position = position % 2;
-
         // crate fragment to place page
         FragmentManager fragmentManager = getSupportFragmentManager();
-//        if (placeholderFragment == null)
-//            placeholderFragment = new PlaceholderFragment();
-////        fragmentManager.beginTransaction()
-////                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-////                .commit();
-//        fragmentManager.beginTransaction().replace(R.id.container, placeholderFragment.newInstance(position + 1)).commit();
-        fragmentManager.beginTransaction().replace(R.id.container, fragments[position]).commit();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
     }
 
     public void onSectionAttached(int number) {
@@ -231,46 +212,46 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         return super.onOptionsItemSelected(item);
     }
 
-//    class PlaceholderFragment extends Fragment {
-//        private static final String ARG_SECTION_NUMBER = "section_number";
-//
-//        public PlaceholderFragment newInstance(int sectionNumber) {
-//            PlaceholderFragment fragment = new PlaceholderFragment();
-//            Bundle args = new Bundle();
-//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//            fragment.setArguments(args);
-//            return fragment;
-//        }
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            int page = getArguments().getInt(ARG_SECTION_NUMBER);
-//            if(page == 1) {
-//                ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_viewpage, container, false);
-//                return rootView;
-//            }
-//            if(page == 2) {
-//                View rootView = inflater.inflate(R.layout.fragment_information, container, false);
-//                return rootView;
-//            }
-//            else if(page == 4){
-//                View rootView = inflater.inflate(R.layout.fragment_setting, container, false);
-//                return rootView;
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        public void onAttach(Activity activity) {
-//            super.onAttach(activity);
-//            ((MainActivity) activity).onSectionAttached(
-//                    getArguments().getInt(ARG_SECTION_NUMBER));
-//        }
-//
-//    }
+    public static class PlaceholderFragment extends Fragment {
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            int page = getArguments().getInt(ARG_SECTION_NUMBER);
+            if(page == 1) {
+                ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_viewpage, container, false);
+                return rootView;
+            }
+            if(page == 2) {
+                View rootView = inflater.inflate(R.layout.fragment_information, container, false);
+                return rootView;
+            }
+            else if(page == 4){
+                View rootView = inflater.inflate(R.layout.fragment_setting, container, false);
+                return rootView;
+            }
+            return null;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+
+    }
 
 }
